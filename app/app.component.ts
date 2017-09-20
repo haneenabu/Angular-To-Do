70ae07786c67abf544ab759508db95b8841c57d2
Guest User <Guest@Epicodus-6D.local>
2017-09-20 10:55:45 -0700

    add pipes

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Task } from './task.model';
 
 //This is a components annotationi
 //This will determine where the components class definition
@@ -8,41 +9,40 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <div class="container">
-    <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
-    <h3>{{currentFocus}}</h3>
-    <ul>
-      <li *ngFor="let currentTask of tasks">{{currentTask.description}}</li>
-      <h1>{{jessica}}</h1>
-    </ul>
-  </div>
+     <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+     <h3>{{currentFocus}}</h3>
+     <task-list [childTaskList] = "masterTaskList" (clickSender)= "editButtonHasBeenClicked($event)"></task-list>
+     <hr>
+     <edit-task [childSelectedTask] = "selectedTask"  (doneButtonClickedSender)="finishedEditing()"></edit-task>
+     <new-task (newTaskSender)="addTask($event)"></new-task>
+   </div>
   `
 })
 
 //This is the components class definition
 //THIS IS HOW A COMPONENT BEHAVES
 export class AppComponent {
-  currentFocus: string= 'Angular Homework';
+  currentFocus: string = 'Angular Homework';
   currentTime = new Date();
-  month: number = this.currentTime.getMonth()+1;
+  month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
+  selectedTask = null;
 
-  tasks: Task []= [
-    new Task('Finish weekend Angular homework for Epicodus course'),
-    new Task('Begin brainstorming possible JavaScript group projects'),
-    new Task('Add README file to last few Angular repos on GitHub')
-  ]
+  masterTaskList: Task[] = [
+   new Task('Finish weekend Angular homework for Epicodus course', 3),
+   new Task('Begin brainstorming possible JavaScript group projects', 2),
+   new Task('Add README file to last few Angular repos on GitHub', 2)
+ ];
 
-  jessica: object [] = this.tasks.filter( function(e) {
-    return !e.done;
-  });
-
-}
-
-export class Task {
-  public done: boolean = false;
-  constructor(public description: string) {
-
+  editButtonHasBeenClicked(clickedTask) {
+    this.selectedTask = clickedTask;
   }
-  // constructor( public inputArray: string[], public homies: string,  ) {}
+
+  finishedEditing() {
+    this.selectedTask = null;
+  }
+  addTask(newTaskFromChild: Task) {
+    this.masterTaskList.push(newTaskFromChild);
+  }
 }
